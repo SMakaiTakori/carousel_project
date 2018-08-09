bd72f1268a5b8e5c5afd19eee6f372c2d566a52c
Makai Takori <makait87@icloud.com>
2018-08-09 12:29:40 -0400

    added js to create static button functionality, updated styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
- $(document).ready(function($){
-   $('.nav button').on('click', function(){ 
-   var whichButton = $(this).data('nav'); 
-   console.log(whichButton);
-   });
+$(document).ready(function($) {
+	
+	var imageBox = $('.slider ul');
+	var imageWidth = $('.slider ul li').first().children('img').width();
+	var imageQuantity = $('.slider ul').children('li').length;
+
+	imageBox.css('width', imageWidth*imageQuantity);
+	console.log(imageWidth);	
+
+	$('.nav button').on('click', function() {
+		var whichButton = $(this).data('nav');
+
+		console.log('working');
+
+		if(whichButton === 'next'){
+			console.log('next')
+			imageBox.animate({
+				'left' : '-500px'
+			})
+
+		}else if (whichButton === 'prev'){
+			console.log('prev')
+			imageBox.animate({
+				'left' : '+500px'
+			})
+		}
+	});
 });
